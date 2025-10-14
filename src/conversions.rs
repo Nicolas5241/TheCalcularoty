03750abc9981d8f1b544b0120b47c84e3742503d
Nicolas Kist Steffen <nicolasdesenvolvedor@gmail.com>
2025-10-13 21:13:03 -0300

    refactor the unit conversion system

--- src/conversions.rs
+++ src/conversions.rs
@@ -1,3 +1,4 @@
+use phf::OrderedMap;
 use slint::{SharedString, ToSharedString};
 use crate::consts::{TWO_PI, HENRY_BASE_TYPE, HERTZ_BASE_TYPE, FARAD_BASE_TYPE};
 use crate::types::*;
@@ -5,55 +6,29 @@ use crate::units::*;
 
 pub fn convert_measure(unit: BFloat, unit_type: &UnitType, unit_label: &SharedString, target_unit: &SharedString) -> BFloat {
 	match unit_type {
-		UnitType::Hertz => hertz_convert(unit, unit_label, target_unit),
-		UnitType::Farad => farad_convert(unit, unit_label, target_unit),
-		UnitType::Henry => henry_convert(unit, unit_label, target_unit),
+		UnitType::Hertz => unit_convert(unit, unit_label, target_unit, &HERTZ_UNITS),
+		UnitType::Farad => unit_convert(unit, unit_label, target_unit, &FARAD_UNITS),
+		UnitType::Henry => unit_convert(unit, unit_label, target_unit, &HENRY_UNITS),
 		UnitType::NotSelected => unimplemented!(),
 	}
 }
 
 pub fn convert_to_base(unit: BFloat, unit_type: &UnitType, unit_label: &SharedString) -> BFloat {
 	match unit_type {
-		UnitType::Hertz => hertz_convert(unit, unit_label, &HERTZ_BASE_TYPE.to_shared_string()),
-		UnitType::Farad => farad_convert(unit, unit_label, &FARAD_BASE_TYPE.to_shared_string()),
-		UnitType::Henry => henry_convert(unit, unit_label, &HENRY_BASE_TYPE.to_shared_string()),
+		UnitType::Hertz => unit_convert(unit, unit_label, &HERTZ_BASE_TYPE.to_shared_string(), &HERTZ_UNITS),
+		UnitType::Farad => unit_convert(unit, unit_label, &FARAD_BASE_TYPE.to_shared_string(), &FARAD_UNITS),
+		UnitType::Henry => unit_convert(unit, unit_label, &HENRY_BASE_TYPE.to_shared_string(), &HENRY_UNITS),
 		UnitType::NotSelected => unimplemented!(),
 	}
 }
 
-fn hertz_convert(hertz: BFloat, unit_label: &SharedString, target_unit: &SharedString) -> BFloat {
-	let target_ratio = *HERTZ_UNITS.get(&target_unit).unwrap();
-	let input_ratio = *HERTZ_UNITS.get(&unit_label).unwrap();
-	let ratio = u64::max(target_ratio, input_ratio) / u64::min(target_ratio, input_ratio);
+fn unit_convert(unit: BFloat, unit_label: &SharedString, target_unit: &SharedString, map: &OrderedMap<&str, i32>) -> BFloat {
+	let target_ratio = *map.get(&target_unit).unwrap();
+	let input_ratio = *map.get(&unit_label).unwrap();
+	let ratio = input_ratio - target_ratio;
 
-	if target_ratio >= input_ratio {
-		return hertz / ratio.into();
+	if ratio < 0 {
+		return unit / 10u32.pow(-ratio as u32).into();
 	}
-	hertz * ratio.into()
-}
-
-fn farad_convert(farad: BFloat, unit_label: &SharedString, target_unit: &SharedString) -> BFloat {
-	let target_ratio = *FARAD_UNITS.get(&target_unit).unwrap();
-	let input_ratio = *FARAD_UNITS.get(&unit_label).unwrap();
-	let ratio = u64::max(target_ratio, input_ratio) / u64::min(target_ratio, input_ratio);
-
-	if target_ratio >= input_ratio {
-		return farad * ratio.into();
-	}
-	farad / ratio.into()
-}
-
-fn henry_convert(henry: BFloat, unit_label: &SharedString, target_unit: &SharedString) -> BFloat {
-	let target_ratio = *HENRY_UNITS.get(&target_unit).unwrap();
-	let input_ratio = *HENRY_UNITS.get(&unit_label).unwrap();
-	let ratio = u64::max(target_ratio, input_ratio) / u64::min(target_ratio, input_ratio);
-
-	if target_ratio >= input_ratio {
-		return henry * ratio.into();
-	}
-	henry / ratio.into()
-}
-
-pub fn hz_to_omega(hz: BFloat) -> BFloat {
-	hz * TWO_PI.clone()
+	unit * 10i32.pow(ratio as u32).into()
 }
--- src/units.rs
+++ src/units.rs
@@ -2,25 +2,30 @@ use phf::{phf_ordered_map, OrderedMap};
 use slint::{SharedString, ToSharedString};
 use crate::traits::MapToSharedStringVec;
 
-pub static HERTZ_UNITS: OrderedMap<&str, u64> = phf_ordered_map! {
-	"Hz" => 1,
-    "kHz" => 1000,
-    "mHz" => 1000000,
-    "gHz" => 1000000000
+pub static HERTZ_UNITS: OrderedMap<&str, i32> = phf_ordered_map! {
+	"Hz" => 0,
+    "kHz" => 3,
+    "MHz" => 6,
+    "GHz" => 9
 };
-pub static FARAD_UNITS: OrderedMap<&str, u64> = phf_ordered_map! {
-	"F" => 1,
-    "mF" => 1000,
-    "μF" => 1000000,
-    "nF" => 1000000000,
-    "pF" => 1000000000000
+pub static FARAD_UNITS: OrderedMap<&str, i32> = phf_ordered_map! {
+	"F" => 0,
+    "mF" => -3,
+    "μF" => -6,
+    "nF" => -9,
+    "pF" => -12
 };
-pub static HENRY_UNITS: OrderedMap<&str, u64> = phf_ordered_map! {
-	"H" => 1,
-    "mH" => 1000,
-    "μH" => 1000000,
-    "nH" => 1000000000,
-    "pH" => 1000000000000
+pub static HENRY_UNITS: OrderedMap<&str, i32> = phf_ordered_map! {
+	"H" => 0,
+    "mH" => -3,
+    "μH" => -6,
+    "nH" => -9,
+    "pH" => -12
+};
+pub static OHM_UNITS: OrderedMap<&str, i32> = phf_ordered_map! {
+	"Ω" => 0,
+	"kΩ" => 3,
+	"MΩ" => 6,
 };
 
 impl<T> MapToSharedStringVec for OrderedMap<&str, T> {
