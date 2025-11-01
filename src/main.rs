ddb7a03e18a82638c5c2b5f656911ec775306aa4
Nicolas Kist Steffen <nicolasdesenvolvedor@gmail.com>
2025-11-01 14:11:36 -0300

    build sysroot and use panic-immediate-abort
    build sysroot and use panic-immediate-abort

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-#![cfg(not(target_os = "android"))]
-
 #![windows_subsystem = "windows"]
+
+use std::thread;
+
+use rodio::{Decoder, OutputStreamBuilder, Sink};
 
 mod logic;
 //mod not_android;
@@ -14,5 +16,27 @@ mod conversions;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     //not_android::run_app()
+	thread::spawn(|| {
+		let file = std::fs::File::open("assets/music.mp3");
+		if file.is_err() {
+			return;
+		}
+		let file = unsafe { file.unwrap_unchecked() };
+
+		let src = Decoder::new_looped(file);
+		let stream_handle = OutputStreamBuilder::open_default_stream();
+
+		if src.is_err() || stream_handle.is_err() {
+			return;
+		}
+
+		let src = unsafe { src.unwrap_unchecked() };
+		let stream_handle = unsafe { stream_handle.unwrap_unchecked() };
+		
+		let sink = Sink::connect_new(stream_handle.mixer());
+		sink.append(src);
+		sink.sleep_until_end();
+	});
+
 	logic::start_ui()
 }
